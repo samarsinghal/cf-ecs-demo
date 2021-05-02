7f059c36caa6194e5b1ac0a6275633f07fb6b9e7
samar singhal <samarsinghal@ssamar-a01.vmware.com>
2021-05-02 13:39:21 -0600

    Initial commit

---
+++ src/test/java/com/example/cfecssample/CfEcsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cfecssample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfEcsSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
